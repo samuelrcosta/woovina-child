8ce08c040508c2910afd7a3dff3d8af931929409
Samuel Rocha Costa <samu.rcosta@gmail.com>
2020-11-18 00:15:20 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_action('wp_enqueue_scripts', 'enqueue_parent_styles', 11);
+
 function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/assets/css/custom.css' );
+    $version = wp_get_theme()->get('Version');
+	$uri = get_stylesheet_directory_uri();
+	
+	// wp_enqueue_style('woovina-niche', $uri."/assets/css/niche-01.css", false, $version);
+	wp_enqueue_style('woovina-child-custom', $uri."/assets/css/custom.css", false, $version);
 }
-?>
